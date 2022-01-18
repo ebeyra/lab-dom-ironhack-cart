f871965198af726af0ff32d28e770500592ade70
Eduardo Beyra <eduardobeyra@hotmail.com>
2022-01-18 18:02:57 -0500

    Cart lab in progress

--- js/index.js
+++ js/index.js
@@ -2,22 +2,43 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  const priceElem = product.querySelector('.price span');
+  const price = Number(priceElem.innerHTML);
 
+  const quantityElem = product.querySelector('.quantity input');
+  const quantity = Number(quantityElem.value);
+
+  const sub = price * quantity;
+
+  const subElem = product.querySelector('.subtotal span');
+  subElem.innerHTML = sub;
+
+  const subTotal = Number(subElem.innerHTML);
+
+  console.log(subTotal);
   //... your code goes here
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  const productAll = document.querySelectorAll('.product');
+  let sum = 0;
+  for (let i = 0; i < productAll.length; i++) {
+    updateSubtotal(productAll[i]);
+    sum += updateSubtotal(productAll[i]);
+  }
 
   // ITERATION 3
   //... your code goes here
+  const totalPriceElem = document.querySelector('#total-value span');
+  totalPriceElem.innerHTML = sum;
 }
 
 // ITERATION 4
